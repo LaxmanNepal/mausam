2fb2fa39f1c237218a6e0866200c40c6c872cc1f
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-09-17 02:23:47 +0300

    bump PWA cache for navbar and city cache upgrade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mausam-v8';
+const CACHE='mausam-v9';
 const ASSETS=['./','./index.html','./style.css','./app.js','./api-compat.js','./enhancements.js','./resilience.js','./districts.js','./intelligence.js','./national.js','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
