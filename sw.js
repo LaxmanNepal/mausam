f716382c994cb7d4925cdf718ab8a15d2e274933
Laxman Nepal <84069546+LaxmanNepal@users.noreply.github.com>
2026-09-17 01:01:02 +0300

    force latest resilient weather client cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='mausam-v5';
+const CACHE='mausam-v6';
 const ASSETS=['./','./index.html','./style.css','./app.js','./api-compat.js','./enhancements.js','./resilience.js','./districts.js','./intelligence.js','./national.js','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='mausam-v6').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
  if(event.request.method!=='GET')return;
  event.respondWith(fetch(event.request).then(response=>{
